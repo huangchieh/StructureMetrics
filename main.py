095cb5b3f03a07038702a94b5806d2a8901c705a
Jie Huang (黄杰) <146966593+huangchieh@users.noreply.github.com>
2024-10-21 10:39:52 +0300

    Add oho (#10)
    * add OHO and cosine similarity
    
    * obtain the output from plot

--- main.py
+++ main.py
@@ -15,26 +15,22 @@ if __name__ == '__main__':
                   'Prediction_a', 'Prediction_b', 'Prediction_c']
     #structures = ['Label']
 
-     
     for structure in structures:
         # Create the output folders
         os.makedirs(os.path.join(baseOut, structure), exist_ok=True)
 
         # Read the samples
         # structure = structures[0]
-    
         samples = read_samples_from_folder(os.path.join('Structures', structure))
         print('Calculating for structure: {}'.format(structure))
-    
         # Common parameters for the plots, and output folder
-        r_max = 4.0
+        r_max = 1.5
         #r_max = 10.0
         #mic = True if structure == 'Label' else False
         mic = False
         bins = 120
         color = '#299035'
         outputFolder = os.path.join(baseOut, structure)
-    
         # --- RDF 
         print('Calulating RDF ...')
         # O-O 
@@ -59,7 +55,7 @@ if __name__ == '__main__':
         firstTwo = False
         onlyAngle = True
         
-        # H-O-H 
+        # H-O-H: Water angle
         y_lim = 0.3 if firstTwo else 0.02
         label = "HOH_dist_{}".format(structure)
         legend = 'H-O-H ({})'.format(structure)
@@ -67,12 +63,18 @@ if __name__ == '__main__':
         np.savez('{}/{}.npz'.format(outputFolder, label), angles=angles)
         plot_angle_distribution(angles, label, legend, color=color, bins=bins, y_lim=y_lim, outfolder=outputFolder)
     
-        # O-O-O 
+        # O-O-O: Related to Au
         y_lim = 0.3 if firstTwo else 0.02
         label = "OOO_dist_{}".format(structure)
         legend = 'O-O-O ({})'.format(structure)
         angles = mean_adf(samples, 'O', 'O', 'O', r_max=r_max, firstTwo=firstTwo, mic=mic, onlyAngle=onlyAngle)
         np.savez('{}/{}.npz'.format(outputFolder, label), angles=angles)
         plot_angle_distribution(angles, label, legend, color=color, bins=bins, y_lim=y_lim, outfolder=outputFolder)
-    
 
+        # O-H-O: H-bond
+        y_lim = 0.3 if firstTwo else 0.02
+        label = "OHO_dist_{}".format(structure)
+        legend = 'O-H-O ({})'.format(structure)
+        angles = mean_adf(samples, 'O', 'H', 'O', r_max=r_max, firstTwo=firstTwo, mic=mic, onlyAngle=onlyAngle)
+        np.savez('{}/{}.npz'.format(outputFolder, label), angles=angles)
+        plot_angle_distribution(angles, label, legend, color=color, bins=bins, y_lim=y_lim, outfolder=outputFolder)
