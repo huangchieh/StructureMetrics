e68df637d5782cb8ffe644c129e10933115e7443
Jie Huang <jie.huang@aalto.fi>
2024-12-13 22:27:19 +0200

    update

--- 02_plotRDFADF.py
+++ 02_plotRDFADF.py
@@ -7,6 +7,8 @@ from water import read_samples_from_folder
 from water import mean_rdf, mean_adf, mean_distance_distribution
 from water import plot_rdf, plot_angle_distribution, plot_distance_distribution
 
+import json
+
 def cosine_similarity(a, b):
     dot_product = np.dot(a, b)
     magnitude_a = np.linalg.norm(a)
@@ -14,200 +16,136 @@ def cosine_similarity(a, b):
     similarity = dot_product / (magnitude_a * magnitude_b)
     return similarity
 
+# Function to write similarities to file
+def write_similarity_to_file(file_path, similarities):
+    with open(file_path, 'w') as f:
+        json.dump(similarities, f, indent=4)
 
 
 if __name__ == '__main__':
     baseOut = 'output'
-    structures = ['Prediction_1', 'Prediction_2', 'Prediction_3']
+    imagePath = 'images'
+    results_file = os.path.join(imagePath, 'similarities.json')
+    structures = [
+        'PPAFM2Exp_CoAll_L60_L0_Elatest',
+        'PPAFM2Exp_CoAll_L60_L0.1_Elatest',
+        'PPAFM2Exp_CoAll_L60_L1_Elatest',
+        'PPAFM2Exp_CoAll_L60_L10_Elatest',
+        'PPAFM2Exp_CoAll_L50_L1_Elatest',
+        'PPAFM2Exp_CoAll_L40_L1_Elatest',
+        'PPAFM2Exp_CoAll_L20_L1_Elatest',
+        'PPAFM2Exp_CoAll_L10_L1_Elatest',
+    ]
 
-    # Common parameters for the plots, and output folder
-    r_max = 3.5
-    outputFolder = os.path.join(baseOut)
-    print('Group 1')
-    # --- RDF 
-    # O-O 
-    data = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, 'P')) 
-    data1 = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, structures[0]))
-    data2 = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, structures[1]))
-    data3 = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, structures[2]))
-    r, gr_OO = data['r'], data['gr']
-    r_1, gr_OO_1 = data1['r'], data1['gr']
-    r_2, gr_OO_2 = data2['r'], data2['gr']
-    r_3, gr_OO_3 = data3['r'], data3['gr']
-    print('Plotting RDF_OO')
-    ylim= 3
-    plot_rdf(r, [gr_OO, gr_OO_1, gr_OO_2, gr_OO_3 ], label='RDF_OO_123',  legend=['Reference', 'OO (Prediction 1)', 'OO (Prediction 2)', 'OO (Prediction 3)'], color=['#299035', '#fc0006', 'orange', 'black'], x_lim=r_max, y_lim=ylim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'], loc="upper left")
-    s1 = cosine_similarity(gr_OO, gr_OO_1)
-    s2 = cosine_similarity(gr_OO, gr_OO_2)
-    s3 = cosine_similarity(gr_OO, gr_OO_3)
-    print('Similarity between Reference and Prediction 1, 2, 3: ', s1, s2, s3)
+    all_similarities = {}
 
-    # O-H 
-    data = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, 'P'))
-    data1 = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, structures[0]))
-    data2 = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, structures[1]))
-    data3 = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, structures[2]))
-    r, gr_OH = data['r'], data['gr']
-    r_1, gr_OH_1 = data1['r'], data1['gr']
-    r_2, gr_OH_2 = data2['r'], data2['gr']
-    r_3, gr_OH_3 = data3['r'], data3['gr']
-    print('Plotting RDF_OH')
-    ylim= 20 
-    plot_rdf(r, [gr_OH, gr_OH_1, gr_OH_2, gr_OH_3 ], label='RDF_OH_123',  legend=['Reference', 'OH (Prediction 1)', 'OH (Prediction 2)', 'OH (Prediction 3)'], color=['#299035', '#fc0006', 'orange', 'black'], x_lim=r_max, y_lim=ylim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'], loc="upper right")
-    s1 = cosine_similarity(gr_OH, gr_OH_1)
-    s2 = cosine_similarity(gr_OH, gr_OH_2)
-    s3 = cosine_similarity(gr_OH, gr_OH_3)
-    print('Similarity between Reference and Prediction 1, 2, 3: ', s1, s2, s3)
+    for structure in structures:
+        print('Calculating for structure: {}'.format(structure))
+        similarity_file = os.path.join(imagePath, structure, 'similarity.json')
+        similarities = {}
 
-    # --- ADF
-    # H-O-H 
-    data = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'P', 'P'))
-    data1 = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'Prediction_1', 'Prediction_1'))
-    data2 = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'Prediction_2', 'Prediction_2'))
-    data3 = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'Prediction_3', 'Prediction_3'))
-    angles = data['angles']
-    angles1 = data1['angles']
-    angles2 = data2['angles']
-    angles3 = data3['angles']
-    print('Plotting ADF_HOH') 
-    y_lim = 0.025
-    bins = 120
-    ns = plot_angle_distribution([angles, angles1, angles2, angles3], label='HOH_dist_123', legend=['Reference', 'HOH (Prediction 1)', 'HOH (Prediction 2)', 'HOH (Prediction 3)'], color=['#299035', '#fc0006', 'orange', 'black'], bins=bins, y_lim=y_lim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'])
-    s1 = cosine_similarity(ns[0], ns[1])
-    s2 = cosine_similarity(ns[0], ns[2])
-    s3 = cosine_similarity(ns[0], ns[3])
-    print('Similarity between Reference and Prediction 1, 2, 3: ', s1, s2, s3)
+        # Common parameters for the plots, and output folder
+        r_max = 3.5
+        outputFolder = os.path.join(baseOut)
 
-    # O-H-O 
-    data = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'P', 'P'))
-    data1 = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'Prediction_1', 'Prediction_1'))
-    data2 = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'Prediction_2', 'Prediction_2'))
-    data3 = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'Prediction_3', 'Prediction_3'))
-    angles = data['angles']
-    angles1 = data1['angles']
-    angles2 = data2['angles']
-    angles3 = data3['angles']
-    print('Plotting ADF_OHO') 
-    y_lim = 0.025
-    bins = 120
-    ns = plot_angle_distribution([angles, angles1, angles2, angles3], label='OHO_dist_123', legend=['Reference', 'OHO (Prediction 1)', 'OHO (Prediction 2)', 'OHO (Prediction 3)'], color=['#299035', '#fc0006', 'orange', 'black'], bins=bins, y_lim=y_lim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'], loc='upper right')
-    s1 = cosine_similarity(ns[0], ns[1])
-    s2 = cosine_similarity(ns[0], ns[2])
-    s3 = cosine_similarity(ns[0], ns[3])
-    print('Similarity between Reference and Prediction 1, 2, 3: ', s1, s2, s3)
+        # --- RDF 
+        # O-O 
+        data = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, 'P')) 
+        r, gr_OO = data['r'], data['gr']
+        data3 = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, 'Ref'))
+        r_3, gr_OO_3 = data3['r'], data3['gr']
+        datac = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, structure))
+        r_c, gr_OO_c = datac['r'], datac['gr']
+        print('Plotting RDF_OO')
+        ylim= 3
+        if not os.path.exists('{}/{}'.format(imagePath, structure)):
+            os.makedirs('{}/{}'.format(imagePath, structure))
+        plot_rdf(r, [gr_OO, gr_OO_3, gr_OO_c], label='RDF_OO',  legend=['Reference', 'O-O (v0)', 'O-O (v1)'], color=['#299035', '#fc0006', '#215ab1'], x_lim=r_max, y_lim=ylim, outfolder='{}/{}'.format(imagePath, structure), style=['bar', 'step', 'step'], loc="upper left")
+        s3 = cosine_similarity(gr_OO, gr_OO_3)
+        sc = cosine_similarity(gr_OO, gr_OO_c)
+        similarity_increase = ((sc - s3) / s3) * 100
+        print('Similarity between Reference and Predictions: ', s3, sc)
+        similarities['RDF_OO'] = {'s3': s3, 'sc': sc, 'similarity_increase': similarity_increase}
 
+        # O-H 
+        data = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, 'P'))
+        r, gr_OH = data['r'], data['gr']
+        data3 = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, "Ref"))
+        r_3, gr_OH_3 = data3['r'], data3['gr']
+        datac = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, structure))
+        r_c, gr_OH_c = datac['r'], datac['gr']
+        print('Plotting RDF_OH')
+        ylim= 20 
+        if not os.path.exists('{}/{}'.format(imagePath, structure)):
+            os.makedirs('{}/{}'.format(imagePath, structure))
+        plot_rdf(r, [gr_OH, gr_OH_3, gr_OH_c], label='RDF_OH',  legend=['Reference', 'O-H (v0)', 'O-H (v1)'], color=['#299035', '#fc0006', '#215ab1'], x_lim=r_max, y_lim=ylim, outfolder='{}/{}'.format(imagePath, structure), style=['bar', 'step', 'step'], loc="upper right")
+        s3 = cosine_similarity(gr_OH, gr_OH_3)
+        sc = cosine_similarity(gr_OH, gr_OH_c)
+        similarity_increase = ((sc - s3) / s3) * 100
+        print('Similarity between Reference and Predictions: ', s3, sc)
+        similarities['RDF_OH'] = {'s3': s3, 'sc': sc, 'similarity_increase': similarity_increase}
 
-    # Theta O-H 
-    data = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'P', 'P'))
-    data1 = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'Prediction_1', 'Prediction_1'))
-    data2 = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'Prediction_2', 'Prediction_2'))
-    data3 = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'Prediction_3', 'Prediction_3'))
-    angles = data['angles']
-    angles1 = data1['angles']
-    angles2 = data2['angles']
-    angles3 = data3['angles']
-    print('Plotting ADF_ThetaOH') 
-    y_lim = 0.035
-    bins = 30 
-    ns = plot_angle_distribution([angles, angles1, angles2, angles3], label='ThetaOH_dist_123', legend=['Reference', 'OH (Prediction 1)', 'OH (Prediction 2)', 'OH (Prediction 3)'], color=['#299035', '#fc0006', 'orange', 'black'], bins=bins, y_lim=y_lim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'], loc='upper right')
-    s1 = cosine_similarity(ns[0], ns[1])
-    s2 = cosine_similarity(ns[0], ns[2])
-    s3 = cosine_similarity(ns[0], ns[3])
-    print('Similarity between Reference and Prediction 1, 2, 3: ', s1, s2, s3)
+        # --- ADF
+        # H-O-H 
+        data = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'P', 'P'))
+        angles = data['angles']
+        data3 = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'Ref', 'Ref'))
+        angles3 = data3['angles']
+        datac = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, structure, structure))
+        anglesc = datac['angles'] 
+        print('Plotting ADF_HOH') 
+        y_lim = 0.025
+        bins = 120
+        if not os.path.exists('{}/{}'.format(imagePath, structure)):
+            os.makedirs('{}/{}'.format(imagePath, structure))
+        ns = plot_angle_distribution([angles, angles3, anglesc], label='HOH_dist', legend=['Reference', 'H-O-H (v0)', 'H-O-H (v1)'], color=['#299035', '#fc0006', '#215ab1'], bins=bins, y_lim=y_lim, outfolder='{}/{}'.format(imagePath, structure), style=['bar',  'step', 'step'])
+        s3 = cosine_similarity(ns[0], ns[1])
+        sc = cosine_similarity(ns[0], ns[2])
+        similarity_increase = ((sc - s3) / s3) * 100
+        print('Similarity between Reference and Predictions: ', s3, sc)
+        similarities['ADF_HOH'] = {'s3': s3, 'sc': sc, 'similarity_increase': similarity_increase}
 
-    # For group 2
-    print('Group 2')
-    structures = ['Prediction_a', 'Prediction_b', 'Prediction_c']
-    # Common parameters for the plots, and output folder
-    r_max = 3.5
-    outputFolder = os.path.join(baseOut)
-    # --- RDF 
-    # O-O 
-    data = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, 'P')) 
-    data1 = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, structures[0]))
-    data2 = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, structures[1]))
-    data3 = np.load('{}/{}/RDF_OO.npz'.format(outputFolder, structures[2]))
-    r, gr_OO = data['r'], data['gr']
-    r_1, gr_OO_1 = data1['r'], data1['gr']
-    r_2, gr_OO_2 = data2['r'], data2['gr']
-    r_3, gr_OO_3 = data3['r'], data3['gr']
-    print('Plotting RDF_OO')
-    ylim= 3
-    plot_rdf(r, [gr_OO, gr_OO_1, gr_OO_2, gr_OO_3 ], label='RDF_OO_abc',  legend=['Reference', 'OO (Prediction a)', 'OO (Prediction b)', 'OO (Prediction c)'], color=['#299035', '#fc0006', 'orange', 'black'], x_lim=r_max, y_lim=ylim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'], loc="upper left")
-    s1 = cosine_similarity(gr_OO, gr_OO_1)
-    s2 = cosine_similarity(gr_OO, gr_OO_2)
-    s3 = cosine_similarity(gr_OO, gr_OO_3)
-    print('Similarity between Reference and Prediction a, b, c: ', s1, s2, s3)
+        # O-H-O 
+        data = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'P', 'P'))
+        angles = data['angles']
+        data3 = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'Ref', 'Ref'))
+        angles3 = data3['angles']
+        datac = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, structure, structure))
+        anglesc = datac['angles']
+        print('Plotting ADF_OHO') 
+        y_lim = 0.025
+        bins = 120
+        if not os.path.exists('{}/{}'.format(imagePath, structure)):
+            os.makedirs('{}/{}'.format(imagePath, structure))
+        ns = plot_angle_distribution([angles, angles3, anglesc], label='OHO_dist', legend=['Reference', 'O-H-O (v0)', 'O-H-O (v1)'], color=['#299035', '#fc0006', '#215ab1'], bins=bins, y_lim=y_lim, outfolder='{}/{}'.format(imagePath, structure), style=['bar', 'step', 'step'], loc='upper right')
+        s3 = cosine_similarity(ns[0], ns[1])
+        sc = cosine_similarity(ns[0], ns[2])
+        similarity_increase = ((sc - s3) / s3) * 100
+        print('Similarity between Reference and Predictions: ', s3, sc)
+        similarities['ADF_OHO'] = {'s3': s3, 'sc': sc, 'similarity_increase': similarity_increase}
 
-    # O-H 
-    data = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, 'P'))
-    data1 = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, structures[0]))
-    data2 = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, structures[1]))
-    data3 = np.load('{}/{}/RDF_OH.npz'.format(outputFolder, structures[2]))
-    r, gr_OH = data['r'], data['gr']
-    r_1, gr_OH_1 = data1['r'], data1['gr']
-    r_2, gr_OH_2 = data2['r'], data2['gr']
-    r_3, gr_OH_3 = data3['r'], data3['gr']
-    print('Plotting RDF_OH')
-    ylim= 20 
-    plot_rdf(r, [gr_OH, gr_OH_1, gr_OH_2, gr_OH_3 ], label='RDF_OH_abc',  legend=['Reference', 'OH (Prediction a)', 'OH (Prediction b)', 'OH (Prediction c)'], color=['#299035', '#fc0006', 'orange', 'black'], x_lim=r_max, y_lim=ylim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'], loc="upper right")
-    s1 = cosine_similarity(gr_OH, gr_OH_1)
-    s2 = cosine_similarity(gr_OH, gr_OH_2)
-    s3 = cosine_similarity(gr_OH, gr_OH_3)
-    print('Similarity between Reference and Prediction a, b, c: ', s1, s2, s3)
+        # Theta O-H 
+        data = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'P', 'P'))
+        angles = data['angles']
+        data3 = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'Ref', 'Ref'))
+        angles3 = data3['angles']
+        datac = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, structure, structure))
+        anglesc = datac['angles']
+        print('Plotting ADF_ThetaOH') 
+        y_lim = 0.035
+        bins = 30 
+        if not os.path.exists('{}/{}'.format(imagePath, structure)):
+            os.makedirs('{}/{}'.format(imagePath, structure))
+        ns = plot_angle_distribution([angles, angles3, anglesc], label='ThetaOH_dist', legend=['Reference', 'O-H (v0)', 'O-H (v1)'], color=['#299035', '#fc0006', '#215ab1'], bins=bins, y_lim=y_lim, outfolder='{}/{}'.format(imagePath, structure), style=['bar', 'step', 'step'], loc='upper right')
+        s3 = cosine_similarity(ns[0], ns[1])
+        sc = cosine_similarity(ns[0], ns[2])
+        similarity_increase = ((sc - s3) / s3) * 100
+        print('Similarity between Reference and Predictions: ', s3, sc)
+        similarities['ADF_ThetaOH'] = {'s3': s3, 'sc': sc, 'similarity_increase': similarity_increase}
 
-    # --- ADF
-    # H-O-H 
-    data = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'P', 'P'))
-    data1 = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'Prediction_a', 'Prediction_a'))
-    data2 = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'Prediction_b', 'Prediction_b'))
-    data3 = np.load('{}/{}/HOH_dist_{}.npz'.format(outputFolder, 'Prediction_c', 'Prediction_c'))
-    angles = data['angles']
-    angles1 = data1['angles']
-    angles2 = data2['angles']
-    angles3 = data3['angles']
-    print('Plotting ADF_HOH') 
-    y_lim = 0.025
-    bins = 120
-    ns = plot_angle_distribution([angles, angles1, angles2, angles3], label='HOH_dist_abc', legend=['Reference', 'HOH (Prediction a)', 'HOH (Prediction b)', 'HOH (Prediction c)'], color=['#299035', '#fc0006', 'orange', 'black'], bins=bins, y_lim=y_lim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'])
-    s1 = cosine_similarity(ns[0], ns[1])
-    s2 = cosine_similarity(ns[0], ns[2])
-    s3 = cosine_similarity(ns[0], ns[3])
-    print('Similarity between Reference and Prediction a, b, c: ', s1, s2, s3)
+        # Store similarities for the current structure
+        all_similarities[structure] = similarities
 
-    # O-H-O 
-    data = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'P', 'P'))
-    data1 = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'Prediction_a', 'Prediction_a'))
-    data2 = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'Prediction_b', 'Prediction_b'))
-    data3 = np.load('{}/{}/OHO_dist_{}.npz'.format(outputFolder, 'Prediction_c', 'Prediction_c'))
-    angles = data['angles']
-    angles1 = data1['angles']
-    angles2 = data2['angles']
-    angles3 = data3['angles']
-    print('Plotting ADF_OHO') 
-    y_lim = 0.025
-    bins = 120
-    ns = plot_angle_distribution([angles, angles1, angles2, angles3], label='OHO_dist_abc', legend=['Reference', 'OHO (Prediction a)', 'OHO (Prediction b)', 'OHO (Prediction c)'], color=['#299035', '#fc0006', 'orange', 'black'], bins=bins, y_lim=y_lim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'], loc='upper right')
-    s1 = cosine_similarity(ns[0], ns[1])
-    s2 = cosine_similarity(ns[0], ns[2])
-    s3 = cosine_similarity(ns[0], ns[3])
-    print('Similarity between Reference and Prediction a, b, c: ', s1, s2, s3)
-
-    # Theta O-H 
-    data = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'P', 'P'))
-    data1 = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'Prediction_a', 'Prediction_a'))
-    data2 = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'Prediction_b', 'Prediction_b'))
-    data3 = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(outputFolder, 'Prediction_c', 'Prediction_c'))
-    angles = data['angles']
-    angles1 = data1['angles']
-    angles2 = data2['angles']
-    angles3 = data3['angles']
-    print('Plotting ADF_ThetaOH') 
-    y_lim = 0.035
-    bins = 30
-    ns = plot_angle_distribution([angles, angles1, angles2, angles3], label='ThetaHO_dist_abc', legend=['Reference', 'OH (Prediction a)', 'OH (Prediction b)', 'OH (Prediction c)'], color=['#299035', '#fc0006', 'orange', 'black'], bins=bins, y_lim=y_lim, outfolder=outputFolder, style=['bar', 'step', 'step', 'step'], loc='upper right')
-    s1 = cosine_similarity(ns[0], ns[1])
-    s2 = cosine_similarity(ns[0], ns[2])
-    s3 = cosine_similarity(ns[0], ns[3])
-    print('Similarity between Reference and Prediction a, b, c: ', s1, s2, s3)
+    # Write all similarities to a single JSON file
+    write_similarity_to_file(results_file, all_similarities)
 
--- 03_hbond_analysis.py
+++ 03_hbond_analysis.py
@@ -8,23 +8,13 @@ from water import mean_rdf, mean_adf, mean_distance_distribution, mean_adf_OH
 from water import plot_rdf, plot_angle_distribution, plot_distance_distribution
 from water import cal_all_hydrogen_bonds, plot_hbond_distance_vs_angle, plot_density_difference 
 
+import json
 
-
-if __name__ == '__main__':
-    baseOut = 'output'
-    #os.makedirs(outputFolder, exist_ok=True)
-    # structures = ['Label', 'P', 'Prediction_1', 'Prediction_2', 'Prediction_3', \
-    #               'Prediction_a', 'Prediction_b', 'Prediction_c']
-    structures = ['P', 'Prediction_3', 'Prediction_c']
-    #structures = ['P', 'Prediction_3', 'Prediction_c']
-
-    for structure in structures:
-        # Create the output folders
-        os.makedirs(os.path.join(baseOut, structure), exist_ok=True)
-
+def calculate(structure):
         # Read the samples
         # structure = structures[0]
-        samples = read_samples_from_folder(os.path.join('Structures', structure))
+        sampleFolder = os.path.join('BatchOutStructures', structure) if (structure == 'Label' or structure == 'P') else os.path.join('BatchOutStructures', structure, 'Prediction_c')
+        samples = read_samples_from_folder(sampleFolder)
         print('Calculating for structure: {}'.format(structure))
 
 
@@ -32,33 +22,62 @@ if __name__ == '__main__':
         print('Finding hydrogen bonds ...')
         #hbonds = cal_all_hydrogen_bonds(samples, z_min=9.5) # For the top layer water
         hbonds = cal_all_hydrogen_bonds(samples)
+        print('Number of hydrogen bonds found: {}'.format(len(hbonds)))
         X_dha, Y_dha, Z_dha = plot_hbond_distance_vs_angle(hbonds, angle_type='dha', label='HBond_dha_{}'.format(structure), cmap='Greens')
         X_hda, Y_hda, Z_hda = plot_hbond_distance_vs_angle(hbonds, angle_type='hda', label='HBond_hda_{}'.format(structure), cmap='Greens')
-        
-        # Plot the density difference
-        if structure == 'P':
-            Z_dha_p, Z_hda_p = Z_dha, Z_hda
-        if structure == 'Prediction_3':
-            Z_dha_diff_3, Z_hda_diff_3 = Z_dha - Z_dha_p, Z_hda - Z_hda_p
-        if structure == 'Prediction_c':
-            Z_dha_diff_c, Z_hda_diff_c = Z_dha - Z_dha_p, Z_hda - Z_hda_p
     
-    # Calculate the mean square error for dha and hda
+        return X_dha, Y_dha, Z_dha, X_hda, Y_hda, Z_hda
+
+if __name__ == '__main__':
+    baseOut = 'images'
+    results_file = os.path.join(baseOut, 'results_Hbond.json')
+
+    _, _, Z_dha_p, _, _, Z_hda_p = calculate('P')
+    X_dha, Y_dha, Z_dha, X_hda, Y_hda, Z_hda = calculate('Ref')
+    Z_dha_diff_3, Z_hda_diff_3 = Z_dha - Z_dha_p, Z_hda - Z_hda_p # This this reference case, results from the orginal PPAFM
     mse_dha_3 = np.mean(Z_dha_diff_3**2)
     mse_hda_3 = np.mean(Z_hda_diff_3**2)
-    mse_dha_c = np.mean(Z_dha_diff_c**2)
-    mse_hda_c = np.mean(Z_hda_diff_c**2)
 
-    # Calculate the percentage improvement
-    percent_improvement_dha = ((mse_dha_3 - mse_dha_c) / mse_dha_3) * 100
-    percent_improvement_hda = ((mse_hda_3 - mse_hda_c) / mse_hda_3) * 100
+    structures = [ 'PPAFM2Exp_CoAll_L60_L0_Elatest', \
+                  'PPAFM2Exp_CoAll_L60_L0.1_Elatest', \
+                  'PPAFM2Exp_CoAll_L60_L1_Elatest', \
+                  'PPAFM2Exp_CoAll_L60_L10_Elatest', \
+                  'PPAFM2Exp_CoAll_L50_L1_Elatest', \
+                  'PPAFM2Exp_CoAll_L40_L1_Elatest', \
+                  'PPAFM2Exp_CoAll_L20_L1_Elatest', \
+                  'PPAFM2Exp_CoAll_L10_L1_Elatest', \
+                  ]
+    results = {}
+    for structure in structures:
+        X_dha, Y_dha, Z_dha, X_hda, Y_hda, Z_hda = calculate(structure)
+        Z_dha_diff_c, Z_hda_diff_c = Z_dha - Z_dha_p, Z_hda - Z_hda_p # This is the results from the Fake AFM
+        mse_dha_c = np.mean(Z_dha_diff_c**2)
+        mse_hda_c = np.mean(Z_hda_diff_c**2)
 
-    # Print the results
-    print('Mean square error between Prediction_3 and Label: dha {:.5f}, hda {:.5f}'.format(mse_dha_3, mse_hda_3))
-    print('Mean square error between Prediction_c and Label: dha {:.5f}, hda {:.5f}'.format(mse_dha_c, mse_hda_c))
-    print('Percentage improvement for Prediction_c compared to Prediction_3: dha {:.2f}%, hda {:.2f}%'.format(percent_improvement_dha, percent_improvement_hda))
+        percent_improvement_dha = ((mse_dha_3 - mse_dha_c) / mse_dha_3) * 100
+        percent_improvement_hda = ((mse_hda_3 - mse_hda_c) / mse_hda_3) * 100
 
-    plot_density_difference(X_dha, Y_dha, Z_dha_diff_3, angle_type='dha', label='HBond_dha_diff_3', cmap='BrBG')
-    plot_density_difference(X_hda, Y_hda, Z_dha_diff_3, angle_type='hda', label='HBond_hda_diff_3', cmap='BrBG')
-    plot_density_difference(X_dha, Y_dha, Z_dha_diff_c, angle_type='dha', label='HBond_dha_diff_c', cmap='BrBG')
-    plot_density_difference(X_hda, Y_hda, Z_hda_diff_c, angle_type='hda', label='HBond_hda_diff_c', cmap='BrBG')
+        # Print the results
+        print('Mean square error between Prediction_3 and Label: dha {:.5f}, hda {:.5f}'.format(mse_dha_3, mse_hda_3))
+        print('Mean square error between Prediction_c and Label: dha {:.5f}, hda {:.5f}'.format(mse_dha_c, mse_hda_c))
+        print('Percentage improvement for Prediction_c compared to Prediction_3: dha {:.2f}%, hda {:.2f}%'.format(percent_improvement_dha, percent_improvement_hda))
+
+        # Store the results in the dictionary
+        results[structure] = {
+            'mse_dha_3': mse_dha_3,
+            'mse_hda_3': mse_hda_3,
+            'mse_dha_c': mse_dha_c,
+            'mse_hda_c': mse_hda_c,
+            'percent_improvement_dha': percent_improvement_dha,
+            'percent_improvement_hda': percent_improvement_hda
+        }
+
+        outfolder = os.path.join(baseOut, structure)
+        plot_density_difference(X_dha, Y_dha, Z_dha_diff_3, angle_type='dha', label='HBond_dha_diff_3', cmap='BrBG', outfolder=outfolder)
+        plot_density_difference(X_hda, Y_hda, Z_dha_diff_3, angle_type='hda', label='HBond_hda_diff_3', cmap='BrBG', outfolder=outfolder)
+        plot_density_difference(X_dha, Y_dha, Z_dha_diff_c, angle_type='dha', label='HBond_dha_diff_c', cmap='BrBG', outfolder=outfolder)
+        plot_density_difference(X_hda, Y_hda, Z_hda_diff_c, angle_type='hda', label='HBond_hda_diff_c', cmap='BrBG', outfolder=outfolder)
+
+    # Write the results to a JSON file
+    with open(results_file, 'w') as f:
+        json.dump(results, f, indent=4)
