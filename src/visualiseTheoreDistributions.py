8d01b43a8fe3a5208e73cc5767e58e81e618c5f9
Jie Huang <jie.huang@aalto.fi>
2025-04-09 23:07:37 +0300

    update

--- src/visualiseTheoreDistributions.py
+++ src/visualiseTheoreDistributions.py
@@ -5,26 +5,35 @@ import matplotlib.pyplot as plt
 
 from water import read_samples_from_folder 
 from water import mean_rdf, mean_adf, mean_distance_distribution, mean_adf_OH
-from water import plot_rdf, plot_angle_distribution, plot_distance_distribution, plot_kde_fill
+from water import plot_rdf, plot_angle_distribution, plot_distance_distribution
+
+from utils import plot_kde_fill
 
 import seaborn as sns
 from scipy.stats import gaussian_kde
 
 if __name__ == '__main__':
     inputFolder = '../data/structures/simulations/'
+    processedFolder = '../processed_data/theory_distributions/'
+    os.makedirs(processedFolder, exist_ok=True)
     baseOut = '../results/theoretical_distributions/'
     structures = ['Label']
+    show = False
     
     for structure in structures:
         # Create the output folders
-        os.makedirs(os.path.join(baseOut, structure), exist_ok=True)
+        figureOut = os.path.join(baseOut, structure)
+        os.makedirs(figureOut, exist_ok=True)
+        # Processed data output
+        npzOut = os.path.join(processedFolder, structure)
+        os.makedirs(npzOut, exist_ok=True)
 
         # Read the samples
         sampleFolder = os.path.join(inputFolder, structure) 
         samples = read_samples_from_folder(sampleFolder)
         print('Calculating for structure: {}'.format(structure))
 
-        fig, axs = plt.subplots(4, 1, figsize=(6, 6))
+        fig, axs = plt.subplots(2, 2, figsize=(8, 8))
         axs = axs.flatten()
 
         # Common parameters for the plots, and output folder
@@ -34,48 +43,60 @@ if __name__ == '__main__':
         bins = 120
         color = '#299035'
         outputFolder = os.path.join(baseOut, structure)
+        alpha = 0.3
 
-        # --- RDF 
+        # O-O distances 
         z_thresholds = {'All': None, 'Top': True, 'Bottom': False}
         colors = {'All': 'green', 'Top': 'red', 'Bottom': 'black'}
-        linestypes = {'All': '-', 'Top': ':', 'Bottom': '-.'}
-        fills = {'All': True, 'Top': True, 'Bottom': False}
+        linestypes = {'All': '-', 'Top': '-', 'Bottom': '-'}
+        fills = {'All': True, 'Top': False, 'Bottom': False}
         for key, value in z_thresholds.items():
-            OO_distances = mean_rdf(samples, 'O', 'O', r_max=3.5, mic=mic, aboveZthres=value, onlyDistances=True)
             label = 'OO_distances_{}_{}'.format(key, structure)
-            legend = 'OO {} ({})'.format(key, structure) if structure != 'P' else 'OO {} (Reference)'.format(key)
-            np.savez('{}/OO_distances_{}.npz'.format(outputFolder, key), distances=OO_distances, r_max=r_max)
-            if key != 'Bottom':
+            legend = '{}'.format(key) 
+            npzFile = '{}/OO_distances_{}.npz'.format(npzOut, key) 
+            if os.path.exists(npzFile):
+                print('Loading OO_distances from file: {}'.format(npzFile))
+                OO_distances = np.load(npzFile)['distances']
+            else:
+                print('Calculating OO_distances ...')
+                OO_distances = mean_rdf(samples, 'O', 'O', r_max=r_max, mic=mic, aboveZthres=value, onlyDistances=True)
+                np.savez(npzFile, distances=OO_distances, r_max=r_max)
+            if key != 'All':
                 axs[0].hist(OO_distances, bins=bins, histtype='step', density=True, linewidth=0.5, color=colors[key], alpha=0.2)
             #sns.kdeplot(OO_distances, ax=axs[0], linewidth=1, label=legend, bw_adjust=1.5, color=colors[key], linestyle=linestypes[key], fill=fills[key], alpha=0.3)
-            plot_kde_fill(ax=axs[0], xmin=0, xmax=r_max, data=OO_distances, color=colors[key], linestyle=linestypes[key], label=legend, fill=fills[key], alpha_fill=0.3)
+            plot_kde_fill(ax=axs[0], xmin=0, xmax=r_max, data=OO_distances, color=colors[key], linestyle=linestypes[key], label=legend, fill=fills[key], alpha_fill=alpha)
         axs[0].set_xlabel(r'$r_\text{OO}$ (Å)')
         axs[0].set_ylabel(r'$\rho(r)$')
-        #axs[0].set_ylim(0, 4.8)
-        axs[0].set_xlim(0, r_max)
+        axs[0].set_ylim(0, 4.8)
+        axs[0].set_xlim(2.2, r_max-0.25)
         axs[0].legend(frameon=False, ncol=1)
         axs[0].tick_params(axis='both', direction='in')
 
-        # O-H all water molecules
+        # O-H distances
         r_max = 1.25
         for key, value in z_thresholds.items():
-            OH_distances = mean_rdf(samples, 'O', 'H', r_max=r_max, mic=mic, aboveZthres=value, onlyDistances=True)
             label = 'OH_distances_{}_{}'.format(key, structure)
             legend = 'OH {} ({})'.format(key, structure) if structure != 'P' else 'OH {} (Reference)'.format(key)
-            #ylim = 150 if structure == 'Label' else 15
-            np.savez('{}/OH_distances_{}.npz'.format(outputFolder, key), distances=OH_distances, r_max=r_max)
-            if key != 'Bottom':
+            npzFile = '{}/OH_distances_{}.npz'.format(npzOut, key)
+            if os.path.exists(npzFile):
+                print('Loading OH_distances from file: {}'.format(npzFile))
+                OH_distances = np.load(npzFile)['distances']
+            else:
+                print('Calculating OH_distances ...')
+                OH_distances = mean_rdf(samples, 'O', 'H', r_max=r_max, mic=mic, aboveZthres=value, onlyDistances=True)
+                np.savez(npzFile, distances=OH_distances, r_max=r_max)
+            if key != 'All':
                 axs[1].hist(OH_distances, bins=bins, histtype='step', density=True, linewidth=0.5, color=colors[key], alpha=0.2)
             #sns.kdeplot(OH_distances, ax=axs[1], linewidth=1, label=legend, bw_adjust=1.5, color=colors[key], linestyle=linestypes[key], fill=fills[key], alpha=0.3)
             plot_kde_fill(ax=axs[1], data=OH_distances, xmin=0, xmax=r_max, color=colors[key], linestyle=linestypes[key], label=legend, fill=fills[key], alpha_fill=0.3)
         axs[1].set_xlabel(r'$r_\text{OH}$ (Å)')
         axs[1].set_ylabel(r'$\rho(r)$')
         #axs[1].set_ylim(0, 120)
-        axs[1].set_xlim(0.9, r_max-0.15)
+        axs[1].set_xlim(0.95, 1.05)
         axs[1].tick_params(axis='both', direction='in')
-        #axs[1].legend()  
-        # --- ADF
-        print('Calculating ADF ...')
+
+        # H-O-H angles
+        print('Plotting H-O-H ...')
         firstTwo = False
         onlyAngle = True
         r_max = 1.25
@@ -83,30 +104,39 @@ if __name__ == '__main__':
         for key, value in z_thresholds.items():
             label = "HOH_dist_{}_{}".format(key, structure)
             legend = 'HOH {} ({})'.format(key, structure) if structure != 'P' else 'HOH {} (Reference)'.format(key)
-            angles = mean_adf(samples, 'H', 'O', 'H', r_max=r_max, firstTwo=firstTwo, mic=mic, onlyAngle=onlyAngle, aboveZthres=value)
-            np.savez('{}/{}.npz'.format(outputFolder, label), angles=angles)
-            #plot_angle_distribution(angles, label, legend, color=color, bins=bins, y_lim=y_lim, outfolder=outputFolder, show=True, figure_size=(3, 3))
-            if key != 'Bottom':
+            npzFile = '{}/HOH_distances_{}.npz'.format(npzOut, key)
+            if os.path.exists(npzFile):
+                print('Loading HOH_distances from file: {}'.format(npzFile))
+                angles = np.load(npzFile)['angles']
+            else:
+                print('Calculating HOH_distances ...')
+                angles = mean_adf(samples, 'H', 'O', 'H', r_max=r_max, firstTwo=firstTwo, mic=mic, onlyAngle=onlyAngle, aboveZthres=value)
+                np.savez(npzFile, angles=angles)
+            if key != 'All':
                 axs[2].hist(angles, bins=bins, histtype='step', density=True, linewidth=0.5, color=colors[key], alpha=0.2)
             #sns.kdeplot(angles, ax=axs[2], linewidth=1, label=legend, bw_adjust=1.5, color=colors[key], linestyle=linestypes[key], fill=fills[key], alpha=0.3)
             plot_kde_fill(ax=axs[2], data=angles, xmin=0, xmax=180, color=colors[key], linestyle=linestypes[key], label=legend, fill=fills[key], alpha_fill=0.3)
         axs[2].set_xlabel(r'$\angle$HOH (degrees)')
-        axs[2].set_xlim(90, 120)
+        axs[2].set_xlim(98, 112)
         axs[2].set_ylabel(r'$\rho(\theta)$')
         axs[2].tick_params(axis='both', direction='in')
-        #axs[2].legend()
 
-        # Theta of OH and z-axis 
+        # ZOH angles 
         r_max = 1.25
         y_lim = 0.04
         bins = 120 
         for key, value in z_thresholds.items():
             label = "Theta_OH_dist_{}_{}".format(key, structure)
             legend = r"$\theta_{{\text{{OH}}}}$ {} ({})".format(key, structure) if structure != 'P' else r"$\theta_{{\text{{OH}}}}$ {} (Reference)".format(key)
-            angles = mean_adf_OH(samples, r_max=r_max, firstTwo=False, mic=False, onlyAngle=True, aboveZthres=value)
-            np.savez('{}/{}.npz'.format(outputFolder, label), angles=angles)
-            #plot_angle_distribution(angles, label, legend, color=color, bins=bins, y_lim=y_lim, outfolder=outputFolder, show=True, figure_size=(3, 3))
-            if key != 'Bottom': 
+            npzFile = '{}/Theta_OH_distances_{}.npz'.format(npzOut, key)
+            if os.path.exists(npzFile):
+                print('Loading Theta_OH_distances from file: {}'.format(npzFile))
+                angles = np.load(npzFile)['angles']
+            else:
+                print('Calculating Theta_OH_distances ...')
+                angles = mean_adf_OH(samples, r_max=r_max, firstTwo=False, mic=False, onlyAngle=True, aboveZthres=value)
+                np.savez(npzFile, angles=angles)
+            if key != 'All': 
                 axs[3].hist(angles, bins=bins, histtype='step', density=True, linewidth=0.5, color=colors[key], alpha=0.2)
             #sns.kdeplot(angles, ax=axs[3], linewidth=1, label=legend, bw_adjust=0.5, color=colors[key], linestyle=linestypes[key], fill=fills[key], alpha=0.3)
             plot_kde_fill(ax=axs[3], data=angles, xmin=0, xmax=180, color=colors[key], linestyle=linestypes[key], label=legend, fill=fills[key], alpha_fill=0.3)
@@ -114,11 +144,9 @@ if __name__ == '__main__':
         axs[3].set_xlim(0, 180)
         axs[3].set_ylabel(r'$\rho(\theta)$')
         axs[3].tick_params(axis='both', direction='in')
-        #axs[3].legend()
 
-        #plt.tight_layout()
-        fig.subplots_adjust(hspace=0.4, wspace=0, left=0.1, bottom=0.1, right=0.95, top=0.98)
-        plt.savefig('{}/RDF_ADF_{}.pdf'.format(outputFolder, structure))
-        plt.savefig('{}/RDF_ADF_{}.png'.format(outputFolder, structure), dpi=300)
-        plt.savefig('{}/RDF_ADF_{}.svg'.format(outputFolder, structure))
-        plt.show()
+        fig.subplots_adjust(hspace=0.2, wspace=0.2, left=0.1, bottom=0.1, right=0.95, top=0.98)
+        plt.savefig('{}/RDF_ADF_{}.pdf'.format(figureOut, structure))
+        plt.savefig('{}/RDF_ADF_{}.png'.format(figureOut, structure), dpi=300)
+        plt.savefig('{}/RDF_ADF_{}.svg'.format(figureOut, structure))
+        if show: plt.show()
