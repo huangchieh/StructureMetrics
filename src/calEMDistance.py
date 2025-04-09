256120f0c34ad3ebe329c3c4952fdc333bf78273
Jie Huang <jie.huang@aalto.fi>
2025-04-09 12:35:04 +0300

    update path

--- src/calEMDistance.py
+++ src/calEMDistance.py
@@ -9,11 +9,13 @@ def write_similarity_to_file(file_path, similarities):
         json.dump(similarities, f, indent=4)
 
 if __name__ == '__main__':
-    baseOut = 'Outputs'
+    inputFolder = '../processed_data/structure_properties'
+    outputFolder = '../processed_data/distribution_distances'
+    os.makedirs(outputFolder, exist_ok=True)
     # Use Label or P as the target answers 
     for ground_truth in ['Label', 'P']:
-        results_file = os.path.join(baseOut, 'similarities_{}.json'.format(ground_truth))
-        structures = [f for f in os.listdir(baseOut) if os.path.isdir(os.path.join(baseOut, f))]
+        results_file = os.path.join(outputFolder, 'similarities_{}.json'.format(ground_truth))
+        structures = [f for f in os.listdir(inputFolder) if os.path.isdir(os.path.join(inputFolder, f))]
     
         all_similarities = {}
         for structure in structures:
@@ -21,46 +23,46 @@ if __name__ == '__main__':
             similarities = {}
     
             # O-O
-            data = np.load('{}/{}/OO_distances.npz'.format(baseOut, ground_truth))
+            data = np.load('{}/{}/OO_distances.npz'.format(inputFolder, ground_truth))
             distances = data['distances']
-            datac = np.load('{}/{}/OO_distances.npz'.format(baseOut, structure))
+            datac = np.load('{}/{}/OO_distances.npz'.format(inputFolder, structure))
             distancesc = datac['distances']
             wdistancec = wasserstein_distance(distances, distancesc)
             similarities['OO_dist'] = {'wdistancec': wdistancec}
     
             # O-H
-            data = np.load('{}/{}/OH_distances.npz'.format(baseOut, ground_truth))
+            data = np.load('{}/{}/OH_distances.npz'.format(inputFolder, ground_truth))
             distances = data['distances']
-            datac = np.load('{}/{}/OH_distances.npz'.format(baseOut, structure))
+            datac = np.load('{}/{}/OH_distances.npz'.format(inputFolder, structure))
             distancesc = datac['distances']
             wdistancec = wasserstein_distance(distances, distancesc)
             similarities['OH_dist'] = {'wdistancec': wdistancec}
     
             # H-O-H 
-            data = np.load('{}/{}/HOH_dist_{}.npz'.format(baseOut, ground_truth, ground_truth))
+            data = np.load('{}/{}/HOH_dist_{}.npz'.format(inputFolder, ground_truth, ground_truth))
             angles = data['angles']
-            datac = np.load('{}/{}/HOH_dist_{}.npz'.format(baseOut, structure, structure))
+            datac = np.load('{}/{}/HOH_dist_{}.npz'.format(inputFolder, structure, structure))
             anglesc = datac['angles'] 
             wdistancec = wasserstein_distance(angles, anglesc)
             similarities['HOH_dist'] = {'wdistancec': wdistancec}
     
             # Theta O-H 
-            data = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(baseOut, ground_truth, ground_truth))
+            data = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(inputFolder, ground_truth, ground_truth))
             angles = data['angles']
-            datac = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(baseOut, structure, structure))
+            datac = np.load('{}/{}/Theta_OH_dist_{}.npz'.format(inputFolder, structure, structure))
             anglesc = datac['angles']
             wdistancec = wasserstein_distance(angles, anglesc)
             similarities['ThetaOH_dist'] = {'wdistancec': wdistancec}
     
             # Hbond
-            data = np.load('{}/{}/Hbonds.npz'.format(baseOut, ground_truth))['distance_angle']
-            datac = np.load('{}/{}/Hbonds.npz'.format(baseOut, structure))['distance_angle']
+            data = np.load('{}/{}/Hbonds.npz'.format(inputFolder, ground_truth))['distance_angle']
+            datac = np.load('{}/{}/Hbonds.npz'.format(inputFolder, structure))['distance_angle']
             wdistancec  = sinkhorn_2d_distance(data, datac)
             similarities['Hbonds'] = {'wdistancec': wdistancec}
     
             # Order parameter 2d
-            data = np.load('{}/{}/OrderP.npz'.format(baseOut, ground_truth))['sg_sk']
-            datac = np.load('{}/{}/OrderP.npz'.format(baseOut, structure))['sg_sk']
+            data = np.load('{}/{}/OrderP.npz'.format(inputFolder, ground_truth))['sg_sk']
+            datac = np.load('{}/{}/OrderP.npz'.format(inputFolder, structure))['sg_sk']
             wdistancec  = sinkhorn_2d_distance(data, datac)
             similarities['OrderP'] = {'wdistancec': wdistancec}
     
