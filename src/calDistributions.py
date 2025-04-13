b8068c557257fcb0dd5d8e03cc314eecf3a3f123
Jie Huang <jie.huang@aalto.fi>
2025-04-13 10:38:14 +0300

    change npz labes

--- src/calDistributions.py
+++ src/calDistributions.py
@@ -32,13 +32,13 @@ if __name__ == '__main__':
         mic = False
         onlyDistances = True
         OO_distances = mean_rdf(samples, 'O', 'O', r_max=r_max, mic=mic, onlyDistances=onlyDistances)
-        np.savez('{}/OO_distances.npz'.format(outputFolder), distances=OO_distances, r_max=r_max)
+        np.savez('{}/OO_distances.npz'.format(outputFolder), OO=OO_distances, r_max=r_max)
     
         # O-H 
         r_max = 1.25
         onlyDistances = True
         OH_distances = mean_rdf(samples, 'O', 'H', r_max=r_max, mic=mic, onlyDistances=onlyDistances)
-        np.savez('{}/OH_distances.npz'.format(outputFolder), distances=OH_distances, r_max=r_max)
+        np.savez('{}/OH_distances.npz'.format(outputFolder), OH=OH_distances, r_max=r_max)
 
         # ADF
         r_max = 1.25
@@ -48,13 +48,13 @@ if __name__ == '__main__':
         # H-O-H: Water angle
         y_lim = 0.3 if firstTwo else 0.02
         angles = mean_adf(samples, 'H', 'O', 'H', r_max=r_max, firstTwo=firstTwo, mic=mic, onlyAngle=onlyAngle)
-        np.savez('{}/{}.npz'.format(outputFolder, "HOH_dist_{}".format(structure)), angles=angles)
+        np.savez('{}/{}.npz'.format(outputFolder, "HOH_dist_{}".format(structure)), HOH=angles, r_max=r_max)
      
         # Theta of OH and z-axis 
         r_max = 1.25
         y_lim = 0.04
         angles = mean_adf_OH(samples, r_max = r_max, firstTwo=False, mic=False, onlyAngle=True, onlyFree=True)
-        np.savez('{}/{}.npz'.format(outputFolder, "Theta_OH_dist_{}".format(structure)), angles=angles)
+        np.savez('{}/{}.npz'.format(outputFolder, "Theta_OH_dist_{}".format(structure)), ZOH=angles, r_max = r_max)
 
         # H-bonds 
         print('Finding hydrogen bonds ...')
@@ -62,7 +62,7 @@ if __name__ == '__main__':
         distances_da = [hb[3] for hb in hbonds]
         angles_dha = [hb[4] for hb in hbonds]
         distance_angle = np.array([distances_da, angles_dha]).T
-        np.savez('{}/Hbonds.npz'.format(outputFolder), distance_angle=distance_angle)
+        np.savez('{}/Hbonds.npz'.format(outputFolder), hbond=hbonds, OO_OHO=distance_angle)
 
         # Order parameters: sg-sk 
         r_max = 3.5
